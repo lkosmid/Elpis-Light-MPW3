62cdf3acf04a52fd342ccf91240b20e22aee9f90
Leonidas Kosmidis <lkosmid@users.noreply.github.com>
2021-11-02 01:24:10 +0100

    adapted regfile based on caravel ibex

--- verilog/rtl/elpis/regfile.v
+++ verilog/rtl/elpis/regfile.v
@@ -4,6 +4,8 @@
 *
 * Copyright © 2020-present. All rights reserved.
 * Authors: Aurora Tomas and Rodrigo Huerta.
+* Leonidas Kosmidis adapted it from the ibex_caravel register_file_ff which is
+* licensed under Apache-2.0
 *
 * This file is licensed under both the BSD-3 license for individual/non-commercial
 * use. Full text of both licenses can be found in LICENSE file.
@@ -31,22 +33,41 @@ module regfile(
 	output[31:0] dest_value
 );
 
-	reg[31:0] registers[31:0];
-	integer i;
+	parameter [31:0] DataWidth = 32;
+	localparam [31:0] ADDR_WIDTH = 5;
+	localparam [31:0] NUM_WORDS = 2 ** ADDR_WIDTH;
+	wire [(NUM_WORDS * DataWidth) - 1:0] rf_reg;
+	reg [((NUM_WORDS - 1) >= 1 ? ((NUM_WORDS - 1) * DataWidth) + (DataWidth - 1) : ((3 - NUM_WORDS) * DataWidth) + (((NUM_WORDS - 1) * DataWidth) - 1)):((NUM_WORDS - 1) >= 1 ? DataWidth : (NUM_WORDS - 1) * DataWidth)] registers;
+	reg [NUM_WORDS - 1:1] we_a_dec;
 
-	always@(negedge clk) begin
-		if (reset) begin
-			for (i=0; i < 32; i = i+1) begin
-				registers[i] <= 0;
-			end
-		end else if (wrd && (addr_d > 0)) begin
-			registers[addr_d] <= d;
+	function automatic [4:0] sv2v_cast_5_unsigned;
+		input reg [4:0] inp;
+		sv2v_cast_5_unsigned = inp;
+	endfunction
+	always @(*) begin : we_a_decoder
+		begin : sv2v_autoblock_2
+			reg [31:0] i;
+			for (i = 1; i < NUM_WORDS; i = i + 1)
+				we_a_dec[i] = (addr_d == sv2v_cast_5_unsigned(i) ? wrd : 1'b0);
 		end
 	end
+	generate
+		genvar i;
+		for (i = 1; i < NUM_WORDS; i = i + 1) begin : g_rf_flops
+			always @(posedge clk or posedge reset)
+				if (reset)
+					registers[((NUM_WORDS - 1) >= 1 ? i : 1 - (i - (NUM_WORDS - 1))) * DataWidth+:DataWidth] <= {DataWidth {1'sb0}};
+				else if (we_a_dec[i])
+					registers[((NUM_WORDS - 1) >= 1 ? i : 1 - (i - (NUM_WORDS - 1))) * DataWidth+:DataWidth] <= d;
+		end
+	endgenerate
+	generate
+			assign rf_reg[0+:DataWidth] = {DataWidth {1'sb0}};
+	endgenerate
 
-	assign a = registers[addr_a];
-	assign b = registers[addr_b];
-	assign dest_value = registers[dest_read];
-
+	assign rf_reg[DataWidth * (((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : ((NUM_WORDS - 1) + ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)) - 1) - (((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS) - 1))+:DataWidth * ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)] = registers[DataWidth * ((NUM_WORDS - 1) >= 1 ? ((NUM_WORDS - 1) >= 1 ? ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : ((NUM_WORDS - 1) + ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)) - 1) - (((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS) - 1) : ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : ((NUM_WORDS - 1) + ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)) - 1)) : 1 - (((NUM_WORDS - 1) >= 1 ? ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : ((NUM_WORDS - 1) + ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)) - 1) - (((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS) - 1) : ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : ((NUM_WORDS - 1) + ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)) - 1)) - (NUM_WORDS - 1)))+:DataWidth * ((NUM_WORDS - 1) >= 1 ? NUM_WORDS - 1 : 3 - NUM_WORDS)];
+	assign a = rf_reg[addr_a * DataWidth+:DataWidth];
+	assign b = rf_reg[addr_b * DataWidth+:DataWidth];
+	assign dest_value = rf_reg[dest_read * DataWidth+:DataWidth];
 endmodule
 
